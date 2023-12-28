0e1a5aa7ed78c327e204fba5e8962d3ce8d5a63b
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-12-28 17:04:50 +0900

    get code block lang and innerText

--- main.ts
+++ main.ts
@@ -3,5 +3,18 @@ import { Plugin } from "obsidian";
 export default class ExecCodeBlock extends Plugin {
   async onload() {
     console.log("onload");
+
+    this.registerMarkdownPostProcessor((element, context) => {
+      const codeblocks = element.findAll("code");
+      for (const codeblock of codeblocks) {
+        const langClassPrefix = "language-";
+        const lang = codeblock.className.substring(
+          codeblock.className.indexOf(langClassPrefix) + langClassPrefix.length
+        );
+        console.log("--- --- ---");
+        console.log("lang is", lang);
+        console.log(codeblock.innerText);
+      }
+    });
   }
 }
